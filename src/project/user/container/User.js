f433d22f8714d518be8fe1cb9b69600e392e49cc
jungeun-p <mrk7753@naver.com>
2022-07-11 18:34:12 +0900

    [ modify ] Update User jsx component

--- src/project/user/container/User.js
+++ src/project/user/container/User.js
@@ -1,4 +1,4 @@
-import { Col, PageHeader, Row } from 'antd';
+import { Col, Descriptions, PageHeader, Row, Typography } from 'antd';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -13,6 +13,9 @@ const User = () => {
         // API로 사용자 정보 가져오기 
         dispatch(actions.fetchUser(name));
     }, [name])
+
+    const isFetched = true;
+    
     return (
         <Row justify="center">
             <Col xs={24} lg={14}>
@@ -20,7 +23,21 @@ const User = () => {
                     onBack={()=> navigate(-1)}
                     title="user information"
                 >
-                    {user?.name}
+                    {user && (
+                        <Descriptions layout="vertical" bordered column={1}>
+                            <Descriptions.Item label="Name">
+                                <Typography>{user.name}</Typography>
+                            </Descriptions.Item>
+                            <Descriptions.Item label="Department">
+                                {user.department}
+                            </Descriptions.Item>
+                            <Descriptions.Item label="Tag">{user.tag}</Descriptions.Item>
+                            <Descriptions.Item label="Log">UpdateLog</Descriptions.Item>
+                        </Descriptions>
+                    )}
+                    {!user && isFetched && (
+                        <Typography.Text>non user</Typography.Text>
+                    )}
                 </PageHeader>
             </Col>
         </Row>
